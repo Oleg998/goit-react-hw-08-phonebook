80d4f06fa515ccb2a72c886b823969aabfcf5e29
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2024-03-02 22:39:20 +0200

    done

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,17 +1,23 @@
-import css from "./NotFound.module.css"
-import imgSrc  from "./error404.png"
-import { Link } from "react-router-dom"
+import css from './NotFound.module.css';
+import imgSrc from './error404.png';
+import { Link } from 'react-router-dom';
 
-const NotFound = ()=> {
-    return (
-          <><div className={css.error}>
-            <img src={imgSrc} alt="error"  className={css.img}/>
-        </div><div className={css.errorMessage}>
-                <h3>Ой! Страница не найдена!</h3>
-                <p>К сожалению мы не можем найти запрошенную вами страницу. Возможно, вы неправильно ввели адрес.</p>
-                <Link to="/">  Go to Home Page</Link>
-            </div></>
-    )
-}
+const NotFound = () => {
+  return (
+    <div className={css.error_container}>
+      <div className={css.error}>
+        <img src={imgSrc} alt="error" className={css.img} />
+      </div>
+      <div className={css.errorMessage}>
+        <h3>Unfortunately, we cannot find the page you requested.</h3>
+        <p> You may have entered the address incorrectly.</p>
+        <Link className={css.link} to="/">
+          
+          Go to Home Page
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
